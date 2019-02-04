5b4870e3cae7861eccb1ecbf986a9e6424f4d03a
Thomas Brushel <thomas@wantable.com>
2019-02-03 19:07:13 -0600

    some updates

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,7 +2,6 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { StaticQuery, graphql } from "gatsby"
-import Header from '../components/header';
 
 function SEO({ description, lang, meta, keywords, title }) {
   return (
