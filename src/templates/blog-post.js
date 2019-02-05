36cee698362e320f9cdc6f31fbfcd6c449ca1fda
Thomas Brushel <thomas@wantable.com>
2019-02-04 20:29:04 -0600

    removed ws

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -8,13 +8,11 @@ export default function Template({ data }) {
   return (
     <Layout>
     <div>
-      
       <hr />
       <h1>{post.frontmatter.title}</h1>
       <h4>
         Posted by {post.frontmatter.author} on {post.frontmatter.date}
       </h4>
-      
       <div dangerouslySetInnerHTML={{ __html: post.html }} />
       <Link to="/blog">Go Back</Link>
     </div>
